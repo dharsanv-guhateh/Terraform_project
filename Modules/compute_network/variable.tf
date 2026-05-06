5b2dae6d222ea684b7cb3a62c1005a4b086f7b75
dharsanv-guhateh <dharsan.v@guhatek.com>
2026-05-06 11:57:25 +0530

    Create variable.tf

---
+++ Modules/compute_network/variable.tf
@@ -0,0 +1,5 @@
+variable "aws_region"    { type = string }
+variable "env"           { type = string }
+variable "vpc_cidr"      { type = string }
+variable "subnet_cidr"   { type = string }
+variable "instance_type" { type = string }
