cd9859884f8ebc28b85b926a92e0b38c09d3a498
dharsanv-guhateh <dharsan.v@guhatek.com>
2026-05-06 11:51:27 +0530

    Create main.tf

---
+++ Modules/compute_network/main.tf
@@ -0,0 +1,72 @@
+# A. DYNAMIC AMI SEARCH (Finds Linux 2 in any region)
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+# B. NETWORK: VPC & GATEWAY
+resource "aws_vpc" "main" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
+  tags                 = { Name = "${var.env}-vpc" }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+  tags   = { Name = "${var.env}-igw" }
+}
+
+# C. NETWORK: ROUTING
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
+# D. NETWORK: SUBNET
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.subnet_cidr
+  map_public_ip_on_launch = true
+  tags                    = { Name = "${var.env}-subnet" }
+}
+
+# E. SECURITY: FIREWALL
+resource "aws_security_group" "sg" {
+  name   = "${var.env}-sg"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# F. COMPUTE: THE EC2 INSTANCE
+resource "aws_instance" "server" {
+  ami                    = data.aws_ami.amazon_linux.id
+  instance_type          = var.instance_type
+  subnet_id              = aws_subnet.public.id
+  vpc_security_group_ids = [aws_security_group.sg.id]
+  tags                   = { Name = "${var.env}-server" }
+}
